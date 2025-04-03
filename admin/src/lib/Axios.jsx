ec27e10fcfb5c670080d703e8e7c01086de430db
[ali edris] <[almusdarthafa999@gmail.com]>
2025-04-03 08:27:33 -0700

    test-again

--- admin/src/lib/Axios.jsx
+++ admin/src/lib/Axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://e-ecommerce-api.onrender.com/api",
   withCredentials: true,
 });
