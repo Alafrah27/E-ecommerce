cc4aef38a6f518e4a3198f261af4ae883b98bc93
Ali Edris <165294446+Alafrah27@users.noreply.github.com>
2025-04-03 07:57:31 -0700

    Update Axios.jsx

--- frontend/src/lib/Axios.jsx
+++ frontend/src/lib/Axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://e-ecommerce-api.onrender.com",
+  baseURL: "https://e-ecommerce-api.onrender.com/api",
   withCredentials: true,
 });
